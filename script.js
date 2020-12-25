958c25b047acf16b8eed04065244e4151ef334c5
Krystian <user@DESKTOP-HNKK2FJ>
2020-12-25 16:25:03 +0100

    added JQuery libary function to fadeIn and fadeOut

--- script.js
+++ script.js
@@ -10,5 +10,16 @@ function show()
 	if(slideNumber > 3){
 		slideNumber = 1;
 	}
-	setTimeout("show()" , 2000);	
+	setTimeout("show()" , 2000);
+	showIn();
+	setTimeout("showOut()" , 1500);
+}
+
+function showIn(){
+	$("#container").fadeIn(500);
+}
+
+function showOut()
+{
+	$("#container").fadeOut(500);
 }
